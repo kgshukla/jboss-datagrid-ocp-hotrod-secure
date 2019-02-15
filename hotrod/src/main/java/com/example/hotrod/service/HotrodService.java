d1a5101fdcfcb90547c0f6823abd4873b48701c0
Kapil Shukla <kgshukla@yahoo.com>
2019-02-15 13:36:45 +0800

    first commit

--- hotrod/src/main/java/com/example/hotrod/service/HotrodService.java
+++ hotrod/src/main/java/com/example/hotrod/service/HotrodService.java
@@ -1,10 +1,13 @@
 package com.example.hotrod.service;
 
+import javax.net.ssl.SSLContext;
+
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.Configuration;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.infinispan.commons.marshall.jboss.GenericJBossMarshaller;
+import org.infinispan.commons.util.SslContextFactory;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,29 +16,80 @@ public class HotrodService {
 	@Value("${DATAGRID_APP_HOTROD_SERVICE_HOST:172.30.154.124}")
 	private String hotrodhost;
 
-  @Value("${DATAGRID_APP_HOTROD_SERVICE_PORT:11333}")
+    @Value("${DATAGRID_APP_HOTROD_SERVICE_PORT:11333}")
 	private String hotrodport;
 	
-	@Value("${USERNAME:user1}")
+	@Value("${cache_username:user1}")
 	private String username;
 	
-	@Value("${PASSWORD:welcome1}")
+	@Value("${cache_passwd:welcome1}")
 	private String password;
+	
+	@Value("${keystore_password:welcome1}")
+	private String keystore_password;
+	
+	@Value("${keystore_alias:selfsigned}")
+	private String keystore_alias;
 	
 	private static final String servername = "jdg-server";
 	
-	private static final String REALM = "ApplicationRealm";
+	private static final String DEFAULT_SSL_PROTOCOL = "TLSv1.2";
+	private static final String DEFAULT_KEYSTORE_TYPE = "JKS";
+	
+	static{
+	    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+	    new javax.net.ssl.HostnameVerifier(){
+
+	        public boolean verify(String hostname,
+	                javax.net.ssl.SSLSession sslSession ) {
+	            return true ;
+	        }
+	    });
+	}
 	
 	public RemoteCacheManager getCacheManager() {
 		
+		
+	SSLContext sslCtx = SslContextFactory.getContext(
+			"/etc/datagrid-secret-volume/..data/jdg-https.jks" ,
+			DEFAULT_KEYSTORE_TYPE,
+			keystore_password.toCharArray(), 
+			null, 
+			keystore_alias, 
+			"/etc/datagrid-secret-volume/..data/jdg-https.jks",
+			DEFAULT_KEYSTORE_TYPE,
+			keystore_password.toCharArray(),
+			DEFAULT_SSL_PROTOCOL,
+			null);	
+		
+		
+
+    
 		ConfigurationBuilder config = new ConfigurationBuilder();
         config.addServers(hotrodhost.concat(":").concat(hotrodport))
                 .marshaller(new GenericJBossMarshaller())
-                .security().authentication()
+                .security()
+                /*
+                .authentication()
+                .serverName(servername) //define server name, should be specified in XML configuration
+                .saslMechanism("DIGEST-MD5") // define SASL mechanism, in this example we use DIGEST with   MD5 hash
+                .username(username)
+                .password(password)
+                */
+                .ssl()
+                .sslContext(sslCtx)
+                .enable()
+                .authentication()
                 .serverName(servername) //define server name, should be specified in XML configuration
                 .saslMechanism("DIGEST-MD5") // define SASL mechanism, in this example we use DIGEST with   MD5 hash
                 .username(username)
                 .password(password);
+//                  .enable()
+//                  .keyAlias("selfsigned")
+//                  .trustStoreFileName(tccl.getResource("jdg-https.jks").getPath())
+//                  .trustStorePassword("changeit".toCharArray())
+//                  .keyStoreFileName(tccl.getResource("jdg-https.jks").getPath())
+//                  .keyStorePassword("changeit".toCharArray());
 
                 //.callbackHandler(new LoginHandler(username, password.toCharArray(), REALM)) // define login handler, implem  entation defined
                 //.enable();
